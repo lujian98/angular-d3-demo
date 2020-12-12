cce9a3f52df8629e8372fba19ac3f41e266a11f2
lujian98 <lujian98@yahoo.com>
2020-12-12 06:50:29 -0800

    idd 007 gauge

--- src/app/d3-demo/d3-demos.module.ts
+++ src/app/d3-demo/d3-demos.module.ts
@@ -21,14 +21,17 @@ import { AppLinearStackedBarChartComponent } from './bar-chart/linear-stacked-ba
 
 import { AppHorizontalBarDemoComponent } from './horizontal-bar-chart/horizontal-bar-chart-demo.component';
 import { AppGroupedHorizontalBarDemoComponent } from './horizontal-bar-chart/grouped-horizontal-bar-chart-demo.component';
-import { AppStackedNormalizedHorizontalBarDemoComponent } from './horizontal-bar-chart/stacked-normalized-horizontal-bar-chart-demo.component';
 import { AppStackedHorizontalBarDemoComponent } from './horizontal-bar-chart/stacked-horizontal-bar-chart-demo.component';
+import { AppStackedNormalizedHorizontalBarDemoComponent } from './horizontal-bar-chart/stacked-normalized-horizontal-bar-chart-demo.component';
 import { AppStockHistoryDemoComponent } from './horizontal-bar-chart/stock-history-demo.component';
 
 import { AppPieChartDemoComponent } from './pie-chart/pie-chart-demo.component';
 import { AppDonutChartDemoComponent } from './pie-chart/donut-chart-demo.component';
 
 import { AppCandleStickChartDemoComponent } from './candle-stick-chart/candle-stick-chart-demo.component';
+
+import { AppRadialGaugeDemoComponent } from './gauge/radial-gauge-demo.component';
+import { AppRadialGaugeDemo2Component } from './gauge/radial-gauge-demo2.component';
 
 const components = [
   AppD3DemosComponent,
@@ -54,7 +57,10 @@ const components = [
   AppPieChartDemoComponent,
   AppDonutChartDemoComponent,
 
-  AppCandleStickChartDemoComponent
+  AppCandleStickChartDemoComponent,
+
+  AppRadialGaugeDemoComponent,
+  AppRadialGaugeDemo2Component
 ];
 
 @NgModule({
